183686572bd687f922de699f987ec51154fd1a58
cdnss <32522595+cdnss@users.noreply.github.com>
2022-03-26 23:51:52 +0700

    update run.js

--- run.js
+++ run.js
@@ -30,7 +30,8 @@ await page.goto('https://jutsuterlarang.blogspot.com/', { waitUntil: 'networkidl
 const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
 const $ = cheerio.load(data);
-  await fs.promises.writeFile('public/index.html', `${$.html()}`);
+let i = $("script").remove();
+  await fs.promises.writeFile('public/index.html', `${i}`);
 
 
  // await page.screenshot({ path: 'public/image.png' });
