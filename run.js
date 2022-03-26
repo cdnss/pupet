27a8a185d63f3b21c797a3946e4a44e7eabcb5c0
cdnss <32522595+cdnss@users.noreply.github.com>
2022-03-27 00:08:50 +0700

    update run.js

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
- const fs = require('fs');
- const cheerio = require('cheerio');
+const fs = require('fs');
+const url = require('url');
+const cheerio = require('cheerio');
 const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 //const express = require('express');
@@ -7,37 +8,44 @@ const puppeteer = require('puppeteer-core');
 //const port = process.env.PORT || 4040;
 
 (async () => {
-  await fs.promises.mkdir('public', { recursive: true });
+ await fs.promises.mkdir('public', {
+  recursive: true
+ });
 
-  const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  } : {
-    args: [],
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+ const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
+  args: chrome.args,
+  executablePath: await chrome.executablePath,
+  headless: chrome.headless
+ }: {
+  args: [],
+  executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+ });
+
+ const page = await browser.newPage();
+
+ await page.setViewport({
+  width: 400,
+  height: 400,
+  deviceScaleFactor: 1
+ });
+ let ddd = await page.url();
+ const current_url = new URL(ddd);
+ const search_params = current_url.searchParams;
+
+ const id = search_params.get('url');
+ if (id) {
+  await page.goto(id, {
+   waitUntil: 'networkidle0'
   });
 
-  const page = await browser.newPage();
+  const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
-  await page.setViewport({
-    width: 400,
-    height: 400,
-    deviceScaleFactor: 1
-  });
-await page.goto('https://jutsuterlarang.blogspot.com/', { waitUntil: 'networkidle0' });
-
-const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-
-const $ = cheerio.load(data);
-$("script").remove();
+  const $ = cheerio.load(data);
+  $("script").remove();
   await fs.promises.writeFile('public/index.html', `${$.html()}`);
-
-
- // await page.screenshot({ path: 'public/image.png' });
-  await browser.close();
+ } else {
+  await fs.promises.writeFile('public/index.html', `add ?url`);
+ }
+}
+ await browser.close();
 })();
-
-
-
-
