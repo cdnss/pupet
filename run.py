ebdf27cc342edbc7a2a6c85f7f2254d3f793100d
cdnss <32522595+cdnss@users.noreply.github.com>
2023-12-05 17:52:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,36 @@
-from requests_html import HTMLSession
-session = HTMLSession()
-r = session.get('https://www.winamax.fr/paris-sportifs/sports/1/1/1')
-r.html.render(timeout=20)
-print(r.html.html)
-session.close()
+import asyncio
+import traceback
+
+from pyppeteer import launch
+from requests_html import HTML
+
+URL = 'https://secure.louisvuitton.com/eng-gb/checkout/review'
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+
+
+async def fetch(url, browser):
+    page = await browser.newPage()
+    await page.setUserAgent(UA)
+
+    try:
+        await page.goto(url, {'waitUntil': 'load'})
+    except:
+        traceback.print_exc()
+    else:
+        return await page.content()
+    finally:
+        await page.close()
+
+
+async def main():
+    browser = await launch(headless=True, args=['--no-sandbox'])
+
+    doc = await fetch(URL, browser)
+    await browser.close()
+
+    html = HTML(html=doc)
+    print(html.links)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
