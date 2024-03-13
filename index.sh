a6e2426283c1c97aeb720411aeb2ae971137da3e
cdnss <32522595+cdnss@users.noreply.github.com>
2024-03-14 02:24:36 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,31 +1,18 @@
-#!/bin/bash
-
-# Update package list
 sudo apt update -y
+sudo apt install qemu qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager
+sudo systemctl enable --now libvirtd
+sudo systemctl enable --now virtlogd
 
-# Download bios64.bin
-wget -O bios64.bin "https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin"
+echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs
 
-# Download ngrok
-wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
-
-# Download Windows ISO
-wget -O win.iso "https://drive.massgrave.dev/en-us_windows_server_2022_updated_dec_2023_x64_dvd_f101ef8f.iso"
-
-# Extract ngrok
-tar -xf ngrok.tgz
-
-# Set ngrok authtoken
-./ngrok authtoken 2dAFDcXtwgCPVr5rBYWZ1XXGyvr_4W5WEYKMgW82FqQGrwhK9
-
-# Start ngrok tunnel
-./ngrok tcp 5900 &
-
-# Install qemu-kvm
-sudo apt install qemu-kvm -y
-
-# Create a raw disk image for Windows
-qemu-img create -f raw win.img 25G
-
-# Run QEMU with specified parameters
-sudo qemu-system-x86_64 -m 8G -smp 2 -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=2 -device e1000,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+sudo modprobe kvm
+docker run -it \
+    -e CPU=Penryn \
+    -e ENABLE_KVM=' ' \
+    -e KVM=' ' \
+    -e CPUID_FLAGS=' ' \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e "DISPLAY=${DISPLAY:-:0.0}" \
+    -v "${PWD}/android.qcow2:/home/arch/dock-droid/android.qcow2" \
+    -p 5555:5555 \
+    sickcodes/dock-droid:latest
