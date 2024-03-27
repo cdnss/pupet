e5fe784009a201749598105d60b7499a6cc693ce
cdnss <32522595+cdnss@users.noreply.github.com>
2024-03-27 23:25:31 +0700

    Update w10.sh

--- w10.sh
+++ w10.sh
@@ -9,7 +9,7 @@ echo Downloading Windows Disk...
 #curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
 echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
