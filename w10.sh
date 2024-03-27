a646f4bcd679eba93087d10cb920afd858cf14f7
cdnss <32522595+cdnss@users.noreply.github.com>
2024-03-27 21:30:14 +0700

    Update and rename requirements.txt to w10.sh

---
+++ w10.sh
@@ -0,0 +1,17 @@
+wget https://raw.githubusercontent.com/cdnss/pupet/main/ngrok.py
+chmod +x ngrok.py
+python ngrok.py
+echo Please wait for installing new 10...
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
+echo Downloading Windows Disk...
+curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
+echo "Windows 10 x64 Lite On Google Colab"
+echo Your VNC IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
+echo Script by fb.com/thuong.hai.581
+echo Starting Windows xxxz...
+sudo qemu-system-x86_64 -vnc :0 -hda w10x64.img -smp cores=2 -m 8096M -machine usb=on -device usb-tablet
