f98380ee64add6388076be6b2e9998da00d37ebd
cdnss <32522595+cdnss@users.noreply.github.com>
2024-12-05 23:09:36 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,18 @@
-import { getScreenshot } from "./_lib/puppeteer";
+import { getScreenshot, getContent } from "./_lib/puppeteer";
 
-module.exports = async (req, res) => {
-  if (process.env.demo == "yes") return res.status(403).send("The public instance of this API is currently disabled. You can deploy your own instance for free on Vercel here: https://s.vercel.app/deploy.");
+module.exports = async function (req, res) {
   if (!req.query.url) return res.status(400).send("No url query specified.");
   if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
   try {
-    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
-    res.setHeader("Content-Type", "image/png");
+    const content = await getContent(req.query.url);
+    res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
-    res.status(200).end(file);
+    res.status(200).end(content);
   } catch (error) {
-    console.error(error);
+    console.error(error)
     res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
   }
-};
+}
 
 function checkUrl(string, hostname) {
   var url = "";
