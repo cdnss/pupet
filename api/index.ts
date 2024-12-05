dc3b1d6a736c836d0adbb132be0cf9e09c27a731
cdnss <32522595+cdnss@users.noreply.github.com>
2024-12-06 00:07:54 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,12 @@
 import { getScreenshot, getContent } from "./_lib/puppeteer";
 
 module.exports = async function (req, res) {
-  try {
-    const content = await getContent("https://fb.com");
+  
+    const content = await getContent("https://xtgem.com");
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
     res.status(200).end(content);
-  } catch (error) {
-    console.error(error)
-    res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
-  }
+  
 }
 
 
